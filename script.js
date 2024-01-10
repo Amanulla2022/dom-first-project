88439aa2846324bb97eb15a30c56f5dfaeb44cc9
Amanulla Mulla <amanullamulla394@gmail.com>
2024-01-10 22:05:32 +0530

    first dom project

---
+++ script.js
@@ -0,0 +1,9 @@
+let divEle = document.getElementById("divContainer");
+
+divEle.style.backgroundColor = "yellow";
+divEle.style.margin = "20px";
+divEle.style.padding = "10px";
+divEle.style.fontSize = "18px";
+divEle.style.fontWeight = "bold";
+divEle.style.height = "200px";
+divEle.style.width = "300px";
